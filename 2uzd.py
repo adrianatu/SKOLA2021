f09a8fad9b9cfca7d2bad7536a4ae552c5656346
adrianatu <adrianatumane2004@inbox.lv>
2021-10-05 15:29:16 +0300

    djsek

---
+++ 2uzd.py
@@ -0,0 +1,17 @@
+#Lietotajs ievada divus veselus skaitlus a un b
+#   iZVADI visus skaitlus saja intervala ieskaitot
+
+#input()
+#cikls
+#range
+
+
+
+a = int(input("Ievadi pirmo skaitli:"))
+b = int(input("Ievadi otru skaitli:"))
+
+#While kamer kkas notiek
+#atstarpe nziime ka esmu ieksa funkcija
+
+for i in range(a,b):
+    print(i)
