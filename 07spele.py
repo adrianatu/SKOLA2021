fefebe4b36f89acae5862191beab03d46f3702c1
adrianatu04 <83282051+adrianatu@users.noreply.github.com>
2021-12-07 15:18:49 +0000

    07

--- 07spele.py
+++ 07spele.py
@@ -1,21 +1,29 @@
-import tkinter as tk 
+import tkinter as tk
+
+window = tk.Tk()
+
+
 def akmens(event):
-  lbl_lietiz ["text"] = "Tava izvēle:akmens"
+  lbl_lietizv ["text"] = "Tava izvēle:akmens"
   dat_izvele()
 
 def papirs(event):
-  lbl_lietiz ["text"] = "Tava izvēle:papīrīts"
+  lbl_lietizv ["text"] = "Tava izvēle:papīrīts"
   dat_izvele()
 
 
 def skeres(event):
-  lbl_lietiz ["text"] = "Tava izvēle:šķēres"
+  lbl_lietizv ["text"] = "Tava izvēle:šķēres"
   dat_izvele()
 
+def dat_izvele():
+  izvēle = ["akmens","šķēres","papīrīts"]
+  datizv = random.choice(izveeles)
+  lbl_datoraizv["text"] = f"Datora izvēle:{datizv}"
 
 def rezultati():
   dators = lbl_datoraizv["text"]
-  lietotajs = lbl_lietiz["text"]
+  lietotajs = lbl_lietizv["text"]
   lietotajs = lietotajs.split()
   
 
@@ -28,25 +36,15 @@ def rezultati():
 
 
 
-def dat_izvele():
-  izvēle = ["akmens","šķēres","papīrīts"]
-  datizv = random.choice(izveeles)
-  lbl_datoraizv["text"] = f"Datora izvēle:{datizv}"
-
-
-  
-  
 window = tk.Tk()
 
-
-window.mainloop()
 
 
 lbl_info = tk.Label(text = "SVEIKI!\n Šī ir spēle akmens,šķēres, papīrīts!")
 
 lbl_darit = tk.Label(text = "Izvēlies kadu no piedāvātajiem variantiem:")
 
-lbl_lietiz = tk.Label()
+lbl_lietizv = tk.Label()
 lbl_datoraizv = tk.Label()
 lbl_rezultats = tk.Label()
 
@@ -58,12 +56,14 @@ btn_akmens.bind("<Button-1>",akmens)
 btn_skeres.bind("<Button-1>",skeres)
 btn_papirs.bind("<Button-1>",papirs)
 
-btn_skeres = tk
+
 
 
 lbl_info.pack(fill = tk.x)
 lbl_darit.pack()
-lbl_lietiz.pack()
+lbl_lietizv.pack()
+lbl_datoraizv.pack()
+lbl_rezultats.pack()
 lbl_datoraizv.pack()
 lbl_rezultats.pack()
 
