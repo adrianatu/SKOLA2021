24802a825038472925648f73e7ba43c2ea8c1085
adrianatu04 <83282051+adrianatu@users.noreply.github.com>
2022-01-11 15:34:49 +0000

    11.01

--- 1101.py
+++ 1101.py
@@ -9,7 +9,7 @@ soup = BeautifulSoup(lapa.content,'html.parser')
 #print(soup.prettyfy())
 print(list(soup.children)
 
-print([type(item) for item in list(soup.children)])
+#print([type(item) for item in list(soup.children)])
 html = list(soup.children)[2]
 
 
@@ -18,5 +18,5 @@ print(list(html.children))
 body = list(html.children)[3]
 print(list(body.children))
 
-p = list(body.children)[1]
-print(p.get_text())
+#p = list(body.children)[1]
+#print(p.get_text())
